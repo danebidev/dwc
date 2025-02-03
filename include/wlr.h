690d2d8adb92a22862c05f957244410548f3a346
danebidev <57720273+danebidev@users.noreply.github.com>
2025-02-04 00:34:28 +0100

    initial commit

---
+++ include/wlr.h
@@ -0,0 +1,7 @@
+#pragma once
+
+// Use this header for all wlr header includes
+
+extern "C" {
+#include <wlr/util/log.h>
+}
---
+++ src/main.cpp
@@ -0,0 +1,5 @@
+#include <getopt.h>
+
+#include "wlr.h"
+
+int main(int argc, char **argv) { wlr_log_init(WLR_DEBUG, NULL); }
